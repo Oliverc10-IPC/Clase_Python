6a0aadaea544b84868baf0db7d30cd6eafd2edd3
Oliver_Cruz <oliver44681598@gmail.com>
2023-02-11 13:08:06 -0600

    Clase 1 completa

---
+++ list_and_dicts.py
@@ -0,0 +1,11 @@
+def run():
+    my_list=[1,"hello",True,4.5]
+    my_dict={"firstname":"Facundo","lastname":"Garcia"}
+super_list=[
+    {"firstname":"Facundo","lastname":"Garcia"},
+    {"firstname":"Oliver","lastname":"Cruz"},
+    {"firstname":"Facundo","lastname":"Garcia"},
+    {"firstname":"Facundo","lastname":"Garcia"}
+]
+if __name__=='__main__':
+    run()
